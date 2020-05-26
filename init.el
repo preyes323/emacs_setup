0eaee6c0194d493e85876720d10f6b9a24bb6d3d
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2020-05-26 11:57:06 +0800

    updates init to use use-package

--- init.el
+++ init.el
@@ -1,8 +1,38 @@
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-(require 'undo-tree)
+(eval-and-compile
+  (require 'package)
+  (setq package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
+                           ("marmalade" . "https://marmalade-repo.org/packages/")
+                           ("melpa" . "https://melpa.org/packages/")))
+  (package-initialize)
+  ;; i always fetch the archive contents on startup and during compilation, which is slow
+  (package-refresh-contents)
+  (unless (package-installed-p 'use-package)
+    (package-install 'use-package))
+  (require 'use-package)
+  ;; i don't really know why this isn't the default...
+  (require 'use-package-ensure)
+  (setq use-package-always-ensure t))
 
-(global-undo-tree-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (js-mode undo-tree use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
-(defun set-jsx-indentation ()
+(defun my-set-jsx-indentation ()
+  "Set indent level to 2"
   (setq-local js-indent-level 2))
-(add-hook 'js-mode-hook 'set-jsx-indentation)
+(add-hook 'js-mode-hook 'my-set-jsx-indentation)
+
+(use-package undo-tree
+  :config
+  (global-undo-tree-mode))
+
+
