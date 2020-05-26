6a222e34bb36c437dbaef566d7127760ec0dbc3b
Victor Paolo Reyes <vpaoloreyes@gmail.com>
2020-05-26 19:54:41 +0800

    adds ivy yasnippet swiper

--- init.el
+++ init.el
@@ -13,19 +13,6 @@
   (require 'use-package-ensure)
   (setq use-package-always-ensure t))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (js-mode undo-tree use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 (defun my-set-jsx-indentation ()
   "Set indent level to 2"
   (setq-local js-indent-level 2))
@@ -34,5 +21,49 @@
 (use-package undo-tree
   :config
   (global-undo-tree-mode))
+
+(use-package yasnippet
+  :ensure t
+  :config
+  (use-package yasnippet-snippets
+    :ensure t)
+  (yas-reload-all)
+  (add-hook 'ruby-mode-hook 'yas-minor-mode)
+  (add-hook 'js-mode-hook 'yas-minor-mode))
+
+(use-package ivy
+  :ensure t
+  :config
+  (use-package swiper
+    :ensure t)
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  ;; enable this if you want `swiper' to use it
+  (setq search-default-mode #'char-fold-to-regexp)
+  (global-set-key "\C-s" 'swiper)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume))
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(setq make-backup-files nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (swiper ivy yasnippet-snippets yasnippet js-mode undo-tree use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+  
 
 
